97d550e5c86620c3e2ae96f7730cedd072524350
electricman991 <braidensommer@yahoo.com>
2024-06-25 01:25:38 -0700

    test code

--- index.js
+++ index.js
@@ -24,7 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  const date = req.params.date
 
+  let inputDate = new Date(date)
+  if (!date) {
+    inputDate = new Date()
+    res.json({unix: date.getTime(), utc: inputDate.toUTCString()})
+  }
+
+  else if (isNaN(inputDate.getTime())) {
+    res.json({error: "Invalid date"})
+  }
+  
+  res.json({unix: inputDate.getTime(), utc: inputDate.toUTCString()})
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
